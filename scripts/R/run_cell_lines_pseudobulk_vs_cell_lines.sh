78d2473069fe1db362d67f7777e362b64af07d22
Ali Oku <aoku@nygenome.org>
2026-04-27 16:11:31 -0400

    Stop tracking unnecessary folders

--- scripts/R/run_cell_lines_pseudobulk_vs_cell_lines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=runMusicCellLines         
-#SBATCH --output=logs/runMusicCellLines.out 
-#SBATCH --error=logs/runMusicCellLines.err   
-#SBATCH --mem=64G
-
-# Run your R script
-Rscript /gpfs/commons/groups/compbio/projects/ao_projects/ml_deconv/scripts/R/cell_lines_pseudobulk_vs_cell_lines_ref.R
