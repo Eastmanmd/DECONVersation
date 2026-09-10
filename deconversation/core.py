35037a49913b68cd858b803c1f76978ea5b43f7d
Ali Oku <aoku@nygenome.org>
2026-09-10 08:59:05 -0400

    updated core deconv function

--- deconversation/core.py
+++ deconversation/core.py
@@ -103,7 +103,8 @@ Parameters
                                                             groupby = cell_type_col,
                                                             output_path = None)
             sig_mat.to_csv(temp_output_dir + "/signature.csv")
-    if mode == "geneformer" and ("ENS" not in sig_mat.index[0]):
+    if (mode == "geneformer" or mode == "scvi") and ("ENS" not in bulk_df.index[0]):
+    #if mode == "geneformer" and ("ENS" not in sig_mat.index[0]):
         print("Signature rows are not ENSG ids, converting...")
         sig_mat.index = preprocessing.gene_id_name_map(gene_list=sig_mat.index, mode="to_ensembl" )
     sig_mat = sig_mat.loc[sig_mat.index.dropna()].T
@@ -119,7 +120,8 @@ Parameters
         bulk_df = pd.read_csv(bulk_df, index_col=0)
         if transpose:
             bulk_df = bulk_df.T
-    if mode == "geneformer" and ("ENS" not in bulk_df.index[0]):
+    if (mode == "geneformer" or mode == "scvi") and ("ENS" not in bulk_df.index[0]):
+    #if mode == "geneformer" and ("ENS" not in bulk_df.index[0]):
         print("Bulk data rows are not ENSG ids, converting...")
         bulk_df.index = preprocessing.gene_id_name_map(gene_list=bulk_df.index, mode="to_ensembl" )
     bulk_df = bulk_df.loc[bulk_df.index.dropna()].T
